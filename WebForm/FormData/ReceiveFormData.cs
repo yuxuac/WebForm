507aaa15d83811de2fbb98d2c5a836bac586ccdd
yuxuac <cui_yuxuan@126.com>
2019-11-05 10:27:20 +0800

    Get and Post methods of Form submit.

--- WebForm/FormData/ReceiveFormData.cs
+++ WebForm/FormData/ReceiveFormData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace FormData
@@ -9,8 +10,32 @@ namespace FormData
     {
         protected override void Post(HttpContext context)
         {
-            var content = GetReceivedContent(context);
-            OK(context.Response, content);
+            //var content = GetReceivedContent(context);
+            //OK(context.Response, content);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<h2>HttpMethod=POST</h2>");
+            sb.AppendLine("<ul>");
+            for (int i = 0; i < context.Request.Params.AllKeys.Length; i++)
+            {
+                sb.AppendLine("<li>" + context.Request.Params.AllKeys[i] + ":" + context.Request.Params[i] + "</li>");
+            }
+            sb.AppendLine("</ul>");
+            OK(context.Response, sb.ToString());
+        }
+
+        protected override void Get(HttpContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<h2>HttpMethod=GET</h2>");
+            sb.AppendLine("<ul>");
+            for (int i = 0; i < context.Request.Params.AllKeys.Length; i++)
+            {
+                sb.AppendLine("<li>" + context.Request.Params.AllKeys[i] + ":" + context.Request.Params[i] + "</li>");
+            }
+            sb.AppendLine("</ul>");
+            OK(context.Response, sb.ToString());
+            //base.Get(context);
         }
     }
 }
